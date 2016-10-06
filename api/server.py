b338de9fba5f9c17a741d2d5fcdec9795faefab0
JulianKnodt <julianknodt@gmail.com>
2016-10-06 00:42:20 -0700

    IT FUCKING WORKS

--- api/server.py
+++ api/server.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Author: DynamicTeapots
 
-from multiprocessing.dummy import Pool as ThreadPool 
+from pandas import DataFrame
 from flask import Flask
 from flask import request
 from flask import Response
@@ -11,15 +11,16 @@ import numpy as np
 from sklearn.decomposition import TruncatedSVD
 from sklearn.preprocessing import Normalizer
 from sklearn.cluster import KMeans
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.preprocessing import Normalizer
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.naive_bayes import MultinomialNB
 import wikipedia
 
 
 app = Flask(__name__)
 
-corpus =[]
+data = DataFrame({'text': [], 'class': []})
 categories = ['cooking', 'bathroom', 'sports', 'hardware', 'cars', 'technology', 'gardening', 'beauty', 'clothing']
 
 
@@ -28,6 +29,10 @@ def getWikiContent (topic):
 
 def getWikiSuggestions (topic, breadth):
   return wikipedia.search(topic, results=breadth)
+
+def naiveSuggestions (topic):
+  return wikipedia.search(topic)
+
 
 
 
@@ -40,6 +45,7 @@ def wikiIterator(topic, depth, *breadth):
   retval = ''
   current = [topic]
   todo = []
+  alltopics = [topic]
   while (depth > 0):
     depth=depth-1
     for item in current:
@@ -51,48 +57,67 @@ def wikiIterator(topic, depth, *breadth):
       todo += getWikiSuggestions(item, breadth)
     print(depth, ' done')
     current = todo
+    alltopics = alltopics + todo
     todo = []
   print(topic + ' researched.')
-  return retval
+  return {data: retval, topics: alltopics}
+
+
+def wikerator(topic, depth, *breadth):
+  rows = []
+  index = []
+  if depth == None:
+    depth = 2
+  if breadth == None:
+    breadth = 5
+  retval = ''
+  current = [topic]
+  todo = []
+  alltopics = [topic]
+  while (depth > 0):
+    depth=depth-1
+    for item in current:
+      print item
+      try:
+        rows.append({'text': getWikiContent(item), 'class': topic})
+        index.append(item)
+      except:
+        pass
+      todo += getWikiSuggestions(item, breadth)
+    print(depth, ' done')
+    current = todo
+    alltopics = alltopics + todo
+    todo = []
+  print(topic + ' researched.')
+  return DataFrame(rows, index=index)
+
+
 
 
 tf = TfidfVectorizer(input='context', analyzer='word', ngram_range=(1,6), lowercase=True, min_df=1, stop_words='english')
 
 for category in categories:
-  corpus = corpus + [wikiIterator(category, 2, 5)]
-
+  data = data.append(wikerator(category, 1, 10))
+data = data.reindex(np.random.permutation(data.index))
+print data
 # Fit trains the vectorizer
 # Transform looks for that
-X = tf.fit_transform(corpus)
+# X = tf.fit_transform(data['text'].values)
 
-svd = TruncatedSVD()
-normalizer = Normalizer(copy=False)
-lsa = make_pipeline(svd, normalizer)
+# svd = TruncatedSVD()
+# normalizer = Normalizer(copy=False)
+# lsa = make_pipeline(svd, normalizer)
 
-X = lsa.fit_transform(X)
+# X = lsa.fit_transform(X)
 
-print(X)
+pipeline = Pipeline([
+  ('vectorizer', TfidfVectorizer(input='context', analyzer='word', ngram_range=(1,6), lowercase=True, min_df=1, stop_words='english')),
+  ('classifier', MultinomialNB())
+  ])
 
-# Initialize the DBSCAN model which will both predict the data given and which we will train with fixed user input
-# Valid values for metrics [‘cityblock’, ‘cosine’, ‘euclidean’, ‘l1’, ‘l2’, ‘manhattan’]
-#Valid values for algorithm are {‘auto’, ‘ball_tree’, ‘kd_tree’, ‘brute’}
-# might want to change leaf size
-# n_jobs is multithreading which takes up more space but since this will be ported to an ec2 it's alright for it to take up all the space
-# might need to increase this eventually
+# km = KMeans(init='k-means++', algorithm='auto', max_iter=600, n_jobs=10, n_clusters=2, verbose=True)
+print(pipeline.fit(data['text'].values, data['class'].values).predict(['cooking cars human what a wonderful world hahaha ofihwpidsghisept test che cut dice chop eat eat eat eat eat cooking chef cooking water boiling stove oven']));
 
-
-
-km = KMeans(init='k-means++', algorithm='auto', max_iter=600, n_jobs=10, n_clusters=len(categories), verbose=True)
-
-km.fit(X)
-# This works really well with small documents?
-# ts = tf.fit_transform(['Cooking Cooking Cooking'])
-
-test = tf.fit_transform(['cooking pots pans running water marble stove fire heat above 220 degrees cooking cooking chef chef chef', '', '', '', '', '', '', '', '', '']);
-
-test = lsa.fit_transform(test)
-
-print(km.predict(test))
 
 # request should be a jsonified string
 # This should contain purely predicts methods
